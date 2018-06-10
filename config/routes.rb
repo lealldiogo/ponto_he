c306757b41e8b48c49b392eea41dbcc9bceb1a41
Diogo Loreto Leal <lealldiogo@gmail.com>
2018-06-10 15:33:34 -0300

    Controllers do devise gerados

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path: 'users'
+  devise_for :users, path: 'users', controllers: { sessions: "users/sessions", confirmations: "users/confirmations", omniauth_callbacks: "users/omniauth_callbacks", passwords: "users/passwords", registrations: "users/registrations", unlocks: "users/unlocks" }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
