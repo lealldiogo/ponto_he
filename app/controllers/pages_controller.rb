5ef57bb3b233a0fa850aad25a710251e2695aa3b
Diogo Loreto Leal <lealldiogo@gmail.com>
2018-06-26 07:23:58 -0300

    O find_or_create_by utilizado com o update parece ser a melhor opção

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,14 @@ class PagesController < ApplicationController
   def home
     @trabalhos = []
     3.times do |i|
-      @trabalhos << Trabalho.new #(data: Date.today - i)
+      # Checar se já tem um trabalho cadastrado na data
+      if Trabalho.new(data: Date.today - i, user_id: current_user.id).valid?
+        @trabalhos << Trabalho.new(data: Date.today - i)
+      else
+        @trabalhos << Trabalho.find_by(data: Date.today - i)
+      end
     end
+    # Encontrei um método no API do Active Record que pode ser útil
+    # find_or_create_by(att: value) returns the first item or creates it and returns it.
   end
 end
