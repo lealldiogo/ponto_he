2591d4c3ef5000741b93ace6df67f177b37968e3
Diogo Loreto Leal <lealldiogo@gmail.com>
2018-11-19 19:59:00 -0300

    Timeoutable configurado

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable and :omniauthable
   devise :database_authenticatable, :recoverable, :trackable,
-  :validatable#, :rememberable #, :registerable
+  :validatable, :timeoutable#, :rememberable #, :registerable
 
   has_many :trabalhos
 
