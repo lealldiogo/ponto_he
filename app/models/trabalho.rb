a79df9628cca8ba061dd2e6aa8475ea6addc136a
Diogo Loreto Leal <lealldiogo@gmail.com>
2018-12-06 20:16:47 -0300

    Adicionada a condição sem hora extra ao banco de dados

--- app/models/trabalho.rb
+++ app/models/trabalho.rb
@@ -4,6 +4,7 @@ class Trabalho < ApplicationRecord
 
   before_update :atualizar_status
   before_update :valor_he_padrao
+  before_update :calcular_jornada
 
   validates :data, presence: true
   validates :data, uniqueness: { scope: :user }
@@ -13,7 +14,6 @@ class Trabalho < ApplicationRecord
 
   validates :entrada, presence: true, on: :update
   validates :saida, presence: true, on: :update
-  validate :calcular_jornada, on: :update
 
   protected
 
@@ -51,10 +51,15 @@ class Trabalho < ApplicationRecord
     #DONE: definir valor de hora extra padrão com base no dia da semana
     #TODO: criar array com datas dos feriados no recife e na paraíba(ignorar municipais nesse caso?)
     #TODO: adicionar um include?(self.data) como condição para valor_he = 100%
+    # feriados = Feriado.all
     if self.data.strftime("%A") == "Sunday"
       self.valor_he = 100
     else
+      # if feriados.include?(self.data)
+        # self.valor_he = 100
+      # else
       self.valor_he = 70
+      # end
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181120232119) do
+ActiveRecord::Schema.define(version: 20181206231420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20181120232119) do
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
     t.string   "veiculo"
+    t.boolean  "sem_he"
     t.index ["obra_id"], name: "index_trabalhos_on_obra_id", using: :btree
     t.index ["user_id"], name: "index_trabalhos_on_user_id", using: :btree
   end
