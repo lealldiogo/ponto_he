7658b0b45bc2dd9e97eb4baf55f232ab094417cb
Diogo Loreto Leal <lealldiogo@gmail.com>
2019-02-03 10:49:47 -0300

    Nome do arquivado editado

--- app/controllers/relatorios_controller.rb
+++ app/controllers/relatorios_controller.rb
@@ -7,10 +7,15 @@ class RelatoriosController < ApplicationController
   def baixar_relatorio
     @user = User.second
     @trabalhos = @user.trabalhos
-    render xlsx: "relatorios/baixar_relatorio", formats: [:xlsx] #, filename: "trabalhos.xlsx"
+    fname = nome_do_arquivo(@user, @trabalhos)
+    render xlsx: "relatorios/baixar_relatorio", formats: [:xlsx], filename: fname
     # respond_to do |format|
     #   format.xlsx {render xlsx: "relatorios/baixar_relatorio", filename: "trabalhos.xlsx"}
     # end
+  end
+
+  def nome_do_arquivo(user, trabalhos)
+    nome_do_arquivo = trabalhos.last.data.strftime("%d-%m") + "_a_" + trabalhos.first.data.strftime("%d-%m") + "_" + user.username.gsub(/\./, '_') + ".xlsx"
   end
 
 end
