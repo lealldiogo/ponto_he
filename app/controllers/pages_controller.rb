8cb79161b3cff73b43bd0ecdb8fe94be6a4a356f
Diogo Loreto Leal <lealldiogo@gmail.com>
2019-02-02 23:25:42 -0300

    Consertando home para admin

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,14 +3,17 @@ class PagesController < ApplicationController
   skip_before_action :is_admin?, only: [:home, :test]
 
   def home
-    # Opção 1: find_or_create_by
-    @trabalhos = []
-    3.times do |i|
-      # Ache ou crie um trabalho do usuário para a data
-      @trabalhos << Trabalho.find_or_create_by(data: Date.today - i, user_id: current_user.id)
+    if current_user.admin?
+      redirect_to admin_path
+    else
+      # Opção 1: find_or_create_by
+      @trabalhos = []
+      3.times do |i|
+        # Ache ou crie um trabalho do usuário para a data
+        @trabalhos << Trabalho.find_or_create_by(data: Date.today - i, user_id: current_user.id)
+      end
+      # Checar quais alertas (? precisa ?)
     end
-
-    # Checar quais alertas (? precisa ?)
   end
 
   def test
