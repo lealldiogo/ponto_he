1491d7a2fcfa066dbf97d5dc815cba96893c50ba
Diogo Loreto Leal <lealldiogo@gmail.com>
2019-05-30 22:53:21 -0300

    filtro por digitação implementado com sucesso

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,3 +4,23 @@
 //= require_tree .
 //= require serviceworker-companion
 //= require bootstrap-datepicker
+
+function retira_acentos(str) {
+  const com_acento = "ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝŔÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿŕ";
+  const sem_acento = "AAAAAAACEEEEIIIIDNOOOOOOUUUUYRsBaaaaaaaceeeeiiiionoooooouuuuybyr";
+  var novastr="";
+  for(i=0; i<str.length; i++) {
+      var troca=false;
+      for (a=0; a<com_acento.length; a++) {
+          if (str.substr(i,1)==com_acento.substr(a,1)) {
+              novastr+=sem_acento.substr(a,1);
+              troca=true;
+              break;
+          }
+      }
+      if (troca==false) {
+          novastr+=str.substr(i,1);
+      }
+  }
+  return novastr;
+}
